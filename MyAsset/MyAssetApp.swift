403d09f254cdab97ea015b4de3e5d814339ef743
TLtlbo <js1436kt@gmail.com>
2023-08-10 21:39:51 +0900

    Initial Commit

---
+++ MyAsset/MyAssetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetApp.swift
+//  MyAsset
+//
+//  Created by 박진성 on 2023/08/10.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
