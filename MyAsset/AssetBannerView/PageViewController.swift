b3ecfaa8ca8737fcf033a18eb33728d1dc20a198
TLtlbo <js1436kt@gmail.com>
2023-08-12 00:12:04 +0900

    UIKit의 PageViewController를 SwiftUI에 적용하여 AssetBanner 뷰 구현

---
+++ MyAsset/AssetBannerView/PageViewController.swift
@@ -0,0 +1,70 @@
+//
+//  PageViewController.swift
+//  MyAsset
+//
+//  Created by 박진성 on 2023/08/11.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page : View>: UIViewControllerRepresentable {
+    
+    var pages : [Page]
+    @Binding var cuurentPage : Int
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        
+        pageViewController.dataSource = context.coordinator
+        pageViewController.delegate = context.coordinator
+        
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers([context.coordinator.controllers[cuurentPage]], direction: .forward, animated: true)
+    }
+    
+    class Coordinator : NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+        var parent : PageViewController
+        var controllers = [UIViewController]()
+        
+        init(_ pageViewController : PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map {UIHostingController(rootView: $0)}
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {return nil}
+            
+            if index == 0 {
+                return controllers.last
+            }
+            
+            return controllers[index - 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {return nil}
+            
+            if index + 1 == controllers.count {
+                return controllers.first
+            }
+            
+            return controllers[index + 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            if completed,
+               let visibleViewController = pageViewController.viewControllers?.first,
+               let index = controllers.firstIndex(of: visibleViewController) {
+                parent.cuurentPage = index
+            }
+        }
+    }
+}
---
+++ MyAsset/Entities/AssetBanner.swift
@@ -0,0 +1,14 @@
+//
+//  AssetBanner.swift
+//  MyAsset
+//
+//  Created by 박진성 on 2023/08/11.
+//
+
+import UIKit
+
+struct AssetBanner {
+    let title : String
+    let description : String
+    let backgroundColor : UIColor
+}
