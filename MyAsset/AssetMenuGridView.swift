f30158c0750841fada128b414f9629a0faa3dd5a
TLtlbo <js1436kt@gmail.com>
2023-08-11 22:52:50 +0900

    Grid View를 이용한 메뉴 섹션 구성 추가

---
+++ MyAsset/AssetMenuGridView.swift
@@ -0,0 +1,37 @@
+//
+//  AssetMenuGridView.swift
+//  MyAsset
+//
+//  Created by 박진성 on 2023/08/10.
+//
+
+import SwiftUI
+
+struct AssetMenuGridView: View {
+    
+    let menuList : [[AssetMenu]] = [
+        [.creditCard, .bankAccount, .investment, .loan],
+        [.insurance, .creditCard, .cash, .realEstate]
+    ]
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            ForEach(menuList, id: \.self) { row in
+                HStack(spacing: 10) {
+                    ForEach(row) { menu in
+                        Button("") {
+                            print("\(menu.title)버튼 tapped")
+                        }
+                        .buttonStyle(AssetMenuButtonStyle(menu: menu))
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AssetMenuGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetMenuGridView()
+    }
+}
---
+++ MyAsset/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MyAsset
+//
+//  Created by 박진성 on 2023/08/11.
+//
+
+import Foundation
